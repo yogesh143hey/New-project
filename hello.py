599f57f7810b004cdff20dd2a890534b33529730
yogesh143hey <yogeshkothakota@gmail.com>
2026-06-18 12:19:11 +0530

    add the area value in rectangle

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
-age = 10
-if age < 18:
-    print("you are a minor")
-else:
-    print("you are an adult.")
+length = int(input("enter the length of the rectangle:"))
+width = int(input("enter the width of the rectangle:"))
+
+area = length * width
+
+print("the area of the rectangle is:", area)
